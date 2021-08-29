5f062766a541794aa4e820f2c28c7b291360149c
brcre001 <brandon.f.creed@gmail.com>
2021-08-29 20:22:05 +0000

    fixed mirgrations

--- migrations/versions/e166ab825e1b_.py
+++ migrations/versions/e166ab825e1b_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 0c331737844b
+Revision ID: e166ab825e1b
 Revises: 
-Create Date: 2021-08-28 17:36:09.034850
+Create Date: 2021-08-29 17:26:15.352055
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '0c331737844b'
+revision = 'e166ab825e1b'
 down_revision = None
 branch_labels = None
 depends_on = None
